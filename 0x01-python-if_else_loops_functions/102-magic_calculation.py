8118045ac64525e647bf332e35f7e45dafc3c898
Halidu Ibrahim <hallydon006@gmail.com>
2023-01-10 12:41:33 +0000

    0x01. Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b, c):
+    """Match bytcode provided by Holberton school."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
--- 0x01-python-if_else_loops_functions/list.h
+++
@@ -1,7 +0,0 @@
-#ifndef LIST_H
-#define LIST_H
-def islower(c):
-def uppercase(str):
-
-
-#endif
